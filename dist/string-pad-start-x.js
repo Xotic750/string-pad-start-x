92d8d132b76b5d881cbadaf7faba968f2c31f2e4
Graham Fairweather <xotic750@gmail.com>
2019-07-19 00:25:05 +0200

    :bookmark: v2.0.7
    Auto build

--- dist/string-pad-start-x.js
+++ dist/string-pad-start-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-18T00:10:33.720Z",
+  "date": "2019-07-18T22:24:59.404Z",
   "describe": "",
   "description": "Pads a string with another string (repeated, if needed).",
   "file": "string-pad-start-x.js",
   "hash": "8405a93b8ca5a115e9e7",
   "license": "MIT",
-  "version": "2.0.6"
+  "version": "2.0.7"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
